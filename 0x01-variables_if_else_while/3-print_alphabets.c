ae4db6dcaaacbb1fddda604423680135db242030
valha-lla <vice.initialize@gmail.com>
2024-01-12 22:07:02 +0100

    print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - print alphabets
+*
+* Description: using the main function
+* Return: 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
