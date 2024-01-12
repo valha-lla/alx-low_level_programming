c06cf49cdeb72d2ca77d21533ab16925dbaf89cf
valha-lla <vice.initialize@gmail.com>
2024-01-12 23:02:14 +0100

    added 5-numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print numbers in base 10
+ *
+ * Description: using the main function
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+	printf("%i", i);
+}
+printf("\n");
+return (0);
+}
