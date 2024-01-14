b1972898964b62e0de908f2737e8c4ea7c529785
valha-lla <vice.initialize@gmail.com>
2024-01-14 15:09:39 +0100

    added 9-timestable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table starting from 0
+ * Return: 0
+ */
+void times_table(void)
+{
+int i;
+int j;
+int result;
+
+for (i = 0; i <= 9; i++)
+{
+	for (j = 0; j <= 9; j++)
+	{
+		result = i * j;
+		if (j == 0)
+		{
+			_putchar(result + '0');
+		}
+		else
+		{
+			result < 10 ? result : result % 10;
+			_putchar(result + '0');
+		}
+	}
+	printf("\n");
+}
+}
