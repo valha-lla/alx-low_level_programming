4cfba9b437168b90f349779d83933396cefa9613
valha-lla <vice.initialize@gmail.com>
2024-01-14 14:20:16 +0100

    fix checks fail

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,13 +10,11 @@ int print_last_digit(int c)
 {
 	if (c > 0 || c == 0)
 	{
-	_putchar (c % 10 + '0');
 	return (c % 10);
 	}
 	else
 	{
 	c = c * -1;
-	_putchar (c % 10 + '0');
 	return (c % 10);
 	}
 }
