a658baef99e8a8eceee2821968786798e90a8c15
valha-lla <vice.initialize@gmail.com>
2024-02-09 16:20:52 +0100

    fixed index failed

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,10 +12,10 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
-	if (n == 0 && index < 11)
+	if (n == 0 && index < 64)
 		return (0);
 
-	for (i = 0; i <= 10; n >>= 1, i++)
+	for (i = 0; i <= 63; n >>= 1, i++)
 	{
 		if (index == i)
 		{
