29f1e012c1037bb655e92e01dfeaf57921d8a631
valha-lla <vice.initialize@gmail.com>
2024-02-14 16:34:37 +0100

    added 2-cp code

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+/**
+ * openr_error - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+void openr_error(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * main - check the code
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, close_error;
+	ssize_t nbytes, wbytes;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	openr_error(file_from, file_to, argv);
+
+	nbytes = 1024;
+	while (nbytes == 1024)
+	{
+		nbytes = read(file_from, buf, 1024);
+		if (nbytes == -1)
+			openr_error(-1, 0, argv);
+		wbytes = write(file_to, buf, nbytes);
+		if (wbytes == -1)
+			openr_error(0, -1, argv);
+	}
+
+	close_error = close(file_from);
+	if (close_error == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	close_error = close(file_to);
+	if (close_error == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
+}
